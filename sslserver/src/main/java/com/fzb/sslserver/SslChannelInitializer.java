40339ca5bd752455f51846e6d25455565f5a9378
zhaoyan <zhaoyan@cccts.com.cn>
2020-04-01 15:28:59 +0800

    init

---
+++ sslserver/src/main/java/com/fzb/sslserver/SslChannelInitializer.java
@@ -0,0 +1,36 @@
+package com.fzb.sslserver;
+
+import java.nio.charset.Charset;
+
+import javax.net.ssl.SSLEngine;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+
+public class SslChannelInitializer extends ChannelInitializer<Channel> {
+    private final SslContext context;
+
+    public SslChannelInitializer(SslContext context) {
+        this.context = context;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        SSLEngine engine = context.newEngine(ch.alloc());
+        engine.setUseClientMode(false);
+        ch.pipeline().addFirst("ssl", new SslHandler(engine));
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("frameDecoder", new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+        pipeline.addLast("frameEncoder", new LengthFieldPrepender(4));  //最大16M
+        pipeline.addLast("decoder", new StringDecoder(Charset.forName("UTF-8")));
+        pipeline.addLast("encoder", new StringEncoder(Charset.forName("UTF-8")));
+        pipeline.addLast("spiderServerBusiHandler", new SpiderServerBusiHandler());
+    }
+}
